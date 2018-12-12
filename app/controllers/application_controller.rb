6a0f48396547fce76187ef725f52631453f890a3
srod0010 <saviofrod@gmail.com>
2018-12-12 13:40:00 -0800

    Fix session cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    helper_method :current_user :logged_in?
+    helper_method :current_user, :logged_in?
 
     private
 
     def login(user)
         user.reset_session_token!
-        session[session_token] = user.session_token
+        session[:session_token] = user.session_token
         @current_user = user
     end
 
     def logout
         @current_user.reset_session_token!
-        session[session_token] = nil
+        session[:session_token] = nil
         @current_user = nil
     end
     
@@ -28,6 +28,6 @@ class ApplicationController < ActionController::Base
     
     def current_user
         return nil unless session[:session_token]
-        @current_user ||= User.find_by(session_token: session[session_token])
+        @current_user ||= User.find_by(session_token: session[:session_token])
     end
 end
