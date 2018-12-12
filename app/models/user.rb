4a28b0ee5d7d945f0bfe51bfa17dc06f59308ef1
srod0010 <saviofrod@gmail.com>
2018-12-12 10:27:42 -0800

    Add user auth methods to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,40 @@
 class User < ApplicationRecord
-    validates :username, :password_digest, :session_token, :city, presence: true
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, :city, presence: true
     validates :password, length: { minimum: 8, allow_nil: true }
     after_initialize :ensure_session_token
+    attr_reader :password
     
     def self.find_user_by_credentials(username, password)
+        user = User.find_by(username: username)
+        
+        if user && user.is_password?(password)
+            return user
+        else
+            return nil
+        end
     end
 
     def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
     end
 
     def is_password?(password)
-    end
-
-    def reset_session_token!
+        BCrypt::Password.new(self.password_digest).is_password?(password)
     end
 
     def generate_session_token
+        SecureRandom.urlsafe_base64
     end
 
     def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save
+        self.session_token
     end
 end
