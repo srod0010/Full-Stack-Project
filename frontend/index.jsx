634c7e1759a247713985695f94097326fc6c6a9e
srod0010 <saviofrod@gmail.com>
2018-12-12 15:29:16 -0800

    Import store

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {signup, login, logout} from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
+    const store = configureStore();
+    window.dispatch = store.dispatch;
+    window.getState = store.getState;
     ReactDOM.render(<h1>hello savi</h1>, root)
 })
 
