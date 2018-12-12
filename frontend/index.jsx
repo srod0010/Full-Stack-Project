856f1b37dc47c014e42b8d139e0b1549f2840de4
srod0010 <saviofrod@gmail.com>
2018-12-12 15:41:26 -0800

    Render root and pass in store

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {signup, login, logout} from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './components/root';
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
     const store = configureStore();
     window.dispatch = store.dispatch;
     window.getState = store.getState;
-    ReactDOM.render(<h1>hello savi</h1>, root)
+    ReactDOM.render(<Root store={store} />, root)
 })
 
 window.signup = signup;
