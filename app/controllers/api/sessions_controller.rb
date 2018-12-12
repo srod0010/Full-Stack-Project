03200baa51630529728a5dc63a895f6f96e8b076
srod0010 <saviofrod@gmail.com>
2018-12-12 13:39:38 -0800

    Fix create method

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::SessionsController < ApplicationController
     def create
-        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        @user = User.find_user_by_credentials(params[:user][:email], params[:user][:password])
 
         if @user
             login(@user)
