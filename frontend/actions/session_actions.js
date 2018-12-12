6a8fa5714ec40303f33c1b68e16eb9991919f173
srod0010 <saviofrod@gmail.com>
2018-12-12 14:00:18 -0800

    Add thunks

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -19,5 +19,25 @@ export const receiveErrors = errors => ({
     errors
 });
 
-////thunks ///
 
+export const signup = user => dispatch => (
+    APIUtil.signup(user).then(user => (
+        dispatch(receiveCurrentUser(user))
+    ), err => (
+        dispatch(receiveErrors(err.responseJSON))
+    ))
+);
+
+export const login = user => dispatch => (
+    APIUtil.login(user).then(user => (
+        dispatch(receiveCurrentUser(user))
+    ), err => (
+        dispatch(receiveErrors(err.responseJSON))
+    ))
+);
+
+export const logout = () => dispatch => (
+    APIUtil.logout().then(user => (
+        dispatch(logoutCurrentUser())
+    ))
+);
