6d4a857fbb3bba9e2458d53c0c1fa0c266305786
srod0010 <saviofrod@gmail.com>
2018-12-12 11:02:15 -0800

    Reset User contoller methods

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,33 +1,3 @@
 class Api::UsersController < ApplicationController
-    protect_from_forgery with: :exception
-    helper_method :current_user :logged_in?
 
-    private
-
-    def login(user)
-        user.reset_session_token!
-        session[session_token] = user.session_token
-        @current_user = user
-    end
-
-    def logout
-        @current_user.reset_session_token!
-        session[session_token] = nil
-        @current_user = nil
-    end
-    
-    def logged_in?
-        !!current_user
-    end
-
-    def require_login
-        unless current_user
-            render json: { base: ['invalid credentials']}, status: 401
-        end
-    end
-    
-    def current_user
-        return nil unless session[:session_token]
-        @current_user ||= User.find_by(session_token: session[session_token])
-    end
 end
