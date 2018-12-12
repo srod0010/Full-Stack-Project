8f37a4e632317daba95fe3fad985044d60c538f2
srod0010 <saviofrod@gmail.com>
2018-12-12 10:12:44 -0800

    Added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+    validates :username, :password_digest, :session_token, :city, presence: true
+    validates :password, length: { minimum: 8, allow_nil: true }
+    after_initialize :ensure_session_token
+    
+    def self.find_user_by_credentials(username, password)
+    end
+
+    def password=(password)
+    end
+
+    def is_password?(password)
+    end
+
+    def reset_session_token!
+    end
+
+    def generate_session_token
+    end
+
+    def ensure_session_token
+    end
 end
