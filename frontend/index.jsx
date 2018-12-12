7ebd573bd39242c04e34a8cbc7547c542b9c6579
srod0010 <saviofrod@gmail.com>
2018-12-12 13:38:56 -0800

    Add ajax methods to window for testing

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {signup, login, logout} from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>hello savi</h1>, root)
 })
+
+window.signup = signup;
+window.login = login;
+window.logout = logout;
