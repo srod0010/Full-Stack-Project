54d714929c861deaf3fcd6217daae6bed644509a
srod0010 <saviofrod@gmail.com>
2018-12-13 09:31:37 -0800

    Fix login form

--- frontend/components/session/login_form_container.jsx
+++ frontend/components/session/login_form_container.jsx
@@ -4,14 +4,18 @@ import { Link } from 'react-router-dom';
 import { login } from '../../actions/session_actions';
 import SessionForm from './session_form';
 
-const mapStateToProps = (state, ownProps) => ({
-    errors: state.errors.session,
-    formType: "Login",
-    navLink: <Link to="/signup">sign up instead</Link>
-})
+const mapStateToProps = ({ errors }) => {
+    return {
+        errors: errors.session,
+        formType: 'login',
+        navLink: <Link to="/signup">sign up instead</Link>,
+    };
+};
 
-const mapDispatchToProps = dispatch => ({
-    processForm: (user) => dispatch(login(user))
-});
+const mapDispatchToProps = dispatch => {
+    return {
+        processForm: (user) => dispatch(login(user)),
+    };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
