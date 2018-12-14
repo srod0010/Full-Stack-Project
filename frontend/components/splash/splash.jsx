90b86cb2394f58141003712f4b497bdeb3a08134
srod0010 <saviofrod@gmail.com>
2018-12-14 11:06:46 -0800

    Change class to className

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,25 +5,25 @@ const splashComponent = () => {
         <div className="outer">
             
             <div className='pic'>
-                <div class="layer">
+                <div className="layer">
                 </div>
             </div>
-            <div class="pic-text">
-                <h1 class="outer-text">New Restaurant You've Been Itching To Try?</h1>
-                <h2 class="inner-text">Grab a Meal With Strangers</h2>
+            <div className="pic-text">
+                <h1 className="outer-text">New Restaurant You've Been Itching To Try?</h1>
+                <h2 className="inner-text">Grab a Meal With Strangers</h2>
             </div>
 
-            <div class="banner">
-                <div class="banner-block first">
+            <div className="banner">
+                <div className="banner-block first">
                     <h1>Try a new restaurant</h1>
                     <h2>Explore the city's best.</h2>
                 </div>
 
-                <div class="banner-block">
+                <div className="banner-block">
                     <h1>Ditch the date</h1>
                     <h2>Meet interesting people.</h2>
                 </div>
-                <div class="banner-block">
+                <div className="banner-block">
                     <h1>Enjoy a night out</h1>
                     <h2>See what happens.</h2>
                 </div>
