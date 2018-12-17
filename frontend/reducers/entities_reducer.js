5f8f97ebe9b977b5af3240c5155b0bb6f24064a1
srod0010 <saviofrod@gmail.com>
2018-12-17 15:42:50 -0800

    Add events reducer to state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import UsersReducer from './users_reducer';
+import EventsReducer from './event_reducer';
 
 
 export default combineReducers({
-    users: UsersReducer
+    users: UsersReducer,
+    events: EventsReducer
 })
