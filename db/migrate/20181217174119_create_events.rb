43ae3e4003d59198f62260443a72c224874610cc
srod0010 <saviofrod@gmail.com>
2018-12-17 10:29:06 -0800

    Add columns and indexes

--- db/migrate/20181217174119_create_events.rb
+++ db/migrate/20181217174119_create_events.rb
@@ -1,8 +1,16 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-
+      t.string :name, null: false
+      t.integer :host_id, null: false
+      t.text :description, null: false
+      t.string :location, null: false
+      t.string :city, null: false
+      t.string :date, null: false
+      t.boolean :open, default: true, null: false
       t.timestamps
     end
+    add_index :events, :name
+    add_index :events, :host_id
   end
 end
