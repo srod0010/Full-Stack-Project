ffadacf78a04484bb389b630c10af4b5a54512ec
srod0010 <saviofrod@gmail.com>
2018-12-17 15:20:04 -0800

    Create events reducer response for receive events action

--- frontend/reducers/event_reducer.js
+++ frontend/reducers/event_reducer.js
@@ -1,2 +1,15 @@
 import { RECEIVE_EVENTS } from '../actions/event_actions';
-import merge from 'merge-lodash';
+import merge from 'lodash/merge';
+
+const eventsReducer = (state = {}, action) => {
+    Object.freeze(state);
+
+    switch(action.type) {
+        case RECEIVE_EVENTS:
+            return action.events;
+        default:
+            return state;
+    }
+}
+
+export default eventsReducer;
