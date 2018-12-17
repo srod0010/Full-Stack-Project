01fbdbf86ea997b60d3c3d01d5fa93cf695a6d71
srod0010 <saviofrod@gmail.com>
2018-12-17 15:42:42 -0800

    Add receive event action and thunk

--- frontend/actions/event_actions.js
+++ frontend/actions/event_actions.js
@@ -1,13 +1,26 @@
 import * as APIUtil from '../util/event_api_util';
 
 export const RECEIVE_EVENTS = "RECEIVE_EVENTS";
+export const RECEIVE_EVENT = "RECEIVE_EVENT";
+
 
 export const receiveEvents = events => ({
     type: RECEIVE_EVENTS,
     events
 })
 
+export const receiveEvent = event => ({
+    type: RECEIVE_EVENT,
+    event
+})
+
 export const fetchEvents = () => dispatch => {
     return APIUtil.fetchEvents()
         .then(events => dispatch(receiveEvents(events)))
+}
+
+
+export const createEvent = event => dispatch => {
+    return APIUtil.createEvent(event)
+        .then(event => dispatch(receiveEvent(event)))
 }
