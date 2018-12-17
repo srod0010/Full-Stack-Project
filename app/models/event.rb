3ee25a0d3e32f3bc40bc0fe8ec371adc9de9c8e6
srod0010 <saviofrod@gmail.com>
2018-12-17 10:28:43 -0800

    Add annotations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string           not null
+#  host_id     :integer          not null
+#  description :text             not null
+#  location    :string           not null
+#  city        :string           not null
+#  date        :string           not null
+#  open        :boolean          default(TRUE), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Event < ApplicationRecord
 end
