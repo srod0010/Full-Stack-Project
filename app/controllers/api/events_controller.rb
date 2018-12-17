6bcf7cd963044ba18de49a8e3a216e9381ec61b8
srod0010 <saviofrod@gmail.com>
2018-12-17 11:17:39 -0800

    Add index, create, and event_params

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,2 +1,26 @@
 class Api::EventsController < ApplicationController
+    before_action :require_login, only: [:create]
+
+    def index
+        @events = Event.all
+        render 'api/events/index'
+    end
+
+    def create
+        @event = Event.new(event_params)
+        @event.host_id = current_user.id
+
+        if @event.save
+            render 'api/events/show'
+        else
+            render json: @event.errors.full_messages, status: 422
+        end
+    end
+
+
+    private
+
+    def event_params
+        params.require(:event).permit(:name, :description, :location, :city, :date, :open)
+    end
 end
