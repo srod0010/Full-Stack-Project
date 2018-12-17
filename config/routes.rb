fca4926039ffdbfdc4cf3afff476cadcc9daa772
srod0010 <saviofrod@gmail.com>
2018-12-17 10:47:35 -0800

    Add all routes for events under api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do 
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+    resources :events, only: [:index, :create, :show, :update, :destroy]
   end
 
   root 'static_pages#root'
