562ad1498be00d86575c99cd3dfcb868ce67938a
srod0010 <saviofrod@gmail.com>
2018-12-17 10:29:20 -0800

    Annotate files

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string           not null
+#  host_id     :integer          not null
+#  description :text             not null
+#  location    :string           not null
+#  city        :string           not null
+#  date        :string           not null
+#  open        :boolean          default(TRUE), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
