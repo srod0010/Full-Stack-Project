5d1bf87bbf021eb5eb383460af196ed0a74e9b6d
srod0010 <saviofrod@gmail.com>
2018-12-17 16:38:58 -0800

    Add fetch and create thunks to window for testings

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {signup, login, logout} from './util/session_api_util';
-import {fetchEvents, createEvent} from './util/event_api_util';
+// import {fetchEvents, createEvent} from './util/event_api_util';
+import {fetchEvents, createEvent} from './actions/event_actions';
 import configureStore from './store/store';
 import Root from './components/root';
 
