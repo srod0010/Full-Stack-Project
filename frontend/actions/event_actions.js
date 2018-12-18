aa10eb4dfacb4c58e7ae13b77159d9bd2d7be157
srod0010 <saviofrod@gmail.com>
2018-12-18 10:22:02 -0800

    Add delete and update thunks

--- frontend/actions/event_actions.js
+++ frontend/actions/event_actions.js
@@ -2,6 +2,7 @@ import * as APIUtil from '../util/event_api_util';
 
 export const RECEIVE_EVENTS = "RECEIVE_EVENTS";
 export const RECEIVE_EVENT = "RECEIVE_EVENT";
+export const REMOVE_EVENT = "REMOVE_EVENT";
 
 
 export const receiveEvents = events => ({
@@ -14,6 +15,12 @@ export const receiveEvent = event => ({
     event
 })
 
+export const removeEvent = event => ({
+    type: REMOVE_EVENT,
+    eventId: event.id
+})
+
+
 export const fetchEvents = () => dispatch => {
     return APIUtil.fetchEvents()
         .then(events => dispatch(receiveEvents(events)))
@@ -23,4 +30,14 @@ export const fetchEvents = () => dispatch => {
 export const createEvent = event => dispatch => {
     return APIUtil.createEvent(event)
         .then(event => dispatch(receiveEvent(event)))
+}
+
+export const updateEvent = event => dispatch => {
+    return APIUtil.updateEvent(event)
+        .then(event => dispatch(receiveEvent(event)))
+}
+
+export const deleteEvent = id => dispatch => {
+    return APIUtil.deleteEvent(id)
+        .then(event => dispatch(removeEvent(event)))
 }
