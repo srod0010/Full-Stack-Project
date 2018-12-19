c1459e9d4f27b5a2fd4ba415ec1e6171913212d6
srod0010 <saviofrod@gmail.com>
2018-12-19 11:53:38 -0800

    Put text inside of photo wrapper

--- frontend/components/events/event_index.jsx
+++ frontend/components/events/event_index.jsx
@@ -21,11 +21,13 @@ class EventIndex extends React.Component {
             <div className="events-wrapper">
 
                 <div className="events-img">
-                    <div className="events-img-cover"></div>
-                    <div className="events-img-text">
-                        <h1>Good meals</h1>
-                        <h3>Are hard to make</h3>
+                    <div className="events-img-cover">
+                        <div className="events-img-text">
+                            <h1>Good meals are hard to make</h1>
+                            <h3>Having a good time with strangers is not</h3>
+                        </div>
                     </div>
+
                 </div>
                 
                 <ul>
