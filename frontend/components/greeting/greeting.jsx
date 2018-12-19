2dfcbd75a38c8b2cf2096f7228d183dd35a4b659
srod0010 <saviofrod@gmail.com>
2018-12-19 10:28:53 -0800

    Change header group to div

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 const Greeting = ({ currentUser, logout }) => {
     const sessionLinks = () => (
         <nav className="login-signup">
+            <Link className="header-button nav" to="/events">Events</Link>
+            <Link className="header-button nav" to="/events/new">Host</Link>
             <button className="greeting-button">
                 <Link to="/login">Login</Link>
             </button>
@@ -16,12 +18,12 @@ const Greeting = ({ currentUser, logout }) => {
         </nav>
     );
     const personalGreeting = () => (
-        <hgroup className="header-group">
+        <div className="header-group">
             {/* <h2 className="header-name">Hi, {currentUser.username}!</h2> */}
             <Link className="header-button nav" to="/events">Events</Link>
             <Link className="header-button nav" to="/events/new">Host</Link>
             <button className="header-button" onClick={logout}>Log Out</button>
-        </hgroup>
+        </div>
     );
 
     return currentUser ? personalGreeting() : sessionLinks();
