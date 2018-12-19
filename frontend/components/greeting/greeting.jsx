b77f572d25bfb3c3fed17c30eb1ba72ec60930da
srod0010 <saviofrod@gmail.com>
2018-12-19 10:11:51 -0800

    Add buttons for create event and event index

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -17,7 +17,9 @@ const Greeting = ({ currentUser, logout }) => {
     );
     const personalGreeting = () => (
         <hgroup className="header-group">
-            <h2 className="header-name">Hi, {currentUser.username}!</h2>
+            {/* <h2 className="header-name">Hi, {currentUser.username}!</h2> */}
+            <Link className="header-button nav" to="/events">Events</Link>
+            <Link className="header-button nav" to="/events/new">Host</Link>
             <button className="header-button" onClick={logout}>Log Out</button>
         </hgroup>
     );
