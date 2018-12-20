6ab2fd1de49741668560b61e6f9a5d726e913ff8
srod0010 <saviofrod@gmail.com>
2018-12-20 11:22:10 -0800

    Redirect to edit page when event is clicked on

--- frontend/components/events/event_index_item.jsx
+++ frontend/components/events/event_index_item.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 const eventIndexItem = (props) => (
-    <div className="event-wrap">
-        <h1 className="event-header">{props.event.name}</h1>
+    <div className="event-wrap" onClick={() => props.history.push(`/events/${props.event.id}/edit`)}>
+        <h1 className="event-header">Join us for: {props.event.name}</h1>
         <ul>
+            <li>{props.event.date}</li>
             <li>{props.event.description}</li>
             <li>{props.event.location}</li>
             <li>{props.event.city}</li>
-            <li>{props.event.date}</li>
             <button onClick={() => props.deleteEvent(props.event.id)}>Delete</button>
         </ul>
     </div>
 )
 
-export default eventIndexItem;
+export default withRouter(eventIndexItem);
