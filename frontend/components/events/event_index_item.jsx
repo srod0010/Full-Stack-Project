0978b9b8ae54c2536c53302d57a12e26d9e5711d
srod0010 <saviofrod@gmail.com>
2018-12-20 17:37:49 -0800

    Change header

--- frontend/components/events/event_index_item.jsx
+++ frontend/components/events/event_index_item.jsx
@@ -3,11 +3,13 @@ import {withRouter, Link} from 'react-router-dom';
 
 
 const eventIndexItem = (props) => (
-    
+
     <div className="event-wrap">
         <h1 className="event-header">Join us for: {props.event.name}</h1>
         <ul>
-            <li>{props.event.date}</li>
+            <div className="event-date">
+                <h3>{props.event.date}</h3>
+            </div>
             <li>{props.event.description}</li>
             <li>{props.event.location}</li>
             <li>{props.event.city}</li>
@@ -15,6 +17,9 @@ const eventIndexItem = (props) => (
             <button onClick={() => props.history.push(`/events/${props.event.id}/edit`)}>Edit</button>
             <Link to={`/events/${props.event.id}`}>show</Link>
         </ul>
+        {/* <div className="event-info-pic">
+            <img src="https://cdn.dribbble.com/users/738700/screenshots/2689482/icon_city_02-02___.png" height="90" width="90"/>
+        </div> */}
     </div>
 )
 
