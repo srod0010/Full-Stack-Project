5be19f1b04a284175e918ff62d3a3952666093cd
srod0010 <saviofrod@gmail.com>
2018-12-21 10:33:25 -0800

    Add event details and description

--- frontend/components/events/event_show.jsx
+++ frontend/components/events/event_show.jsx
@@ -7,6 +7,18 @@ class EventShow extends React.Component {
         let Id = this.props.match.params.eventId
         this.props.fetchEvent(Id)
     }
+
+    getDate() {
+        let newDate = this.props.event.date;
+        let dateShow = newDate.slice(0,10);
+        return dateShow;
+    }
+
+    getTime() {
+        let time = this.props.event.date;
+        let timeSlice = time.slice(11);
+        return timeSlice;
+    }
     render() {
         let event = this.props.event
         if (!event) {
@@ -17,7 +29,15 @@ class EventShow extends React.Component {
                 <div className="show-content-container">
                     <div className="left-show">
                         <div className="left-first">
-                            <li>{event.name}</li>
+                            <div className="left-first-inner">
+                                <h2 className="event-name">Join us for {event.name}</h2>
+                                <h3 className="left-date"> 🗓 {this.getDate()}</h3>
+                                <h3 className="left-date"> ⏰ {this.getTime()} PM</h3>
+                                <h3 className="left-date"> 📍 {this.props.event.location}</h3>
+                                <h3 className="left-date"> 🌇 {this.props.event.city}</h3>
+                                <h3 className="left-date"> 💯 Send to a friend 💯 </h3>
+                            </div>
+                            
                         </div>
 
                         <div className="left-second">
@@ -27,7 +47,21 @@ class EventShow extends React.Component {
                         </div>
 
                         <div className="left-third">
-                            
+                            <h3 className="third-header">What exactly is meal time? <br/>
+                            😋
+                            </h3>
+                            <p className="show-p">
+Meal Time is where five-ish strangers sit at a cafe with a Host to have a two hour conversation. It's not about anything in particular.
+                            </p>
+                            <p className="show-p">
+You're meant to know pretty much nothing about the people that come, aside from the fact that they all signed up for this — just like you. It's self-selecting in that sense.
+                            </p>
+                            <p className="show-p">
+However, it may bring you comfort to know a little something about your Host. If so, just keep reading
+                            </p>
+
+                            <h1 className="show-last-pointer"> 👉👉👉 </h1>
+
                         </div>
                     </div>
 
