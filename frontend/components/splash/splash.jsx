0a5a6ea5e0880c1aed7f8e6d2f8af6bc1e032b79
Savio Rodrigues <saviofrod@gmail.com>
2019-01-29 11:13:07 -0800

    Inital styling for bottom of splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -63,7 +63,6 @@ const splashComponent = () => {
                   So often, we only meet people through work, school, or
                   friends of friends. And even then, it’s questionable. So
                   basically, we manufacture serendipity
-                  
                 </p>
               </div>
 
@@ -73,18 +72,83 @@ const splashComponent = () => {
             <div className="greeting-row">
               <div className="greeting-img third" />
               <div className="greeting-writing">
-                <h2>We won't meet otherwise.</h2>
+                <h2>Your hands are made for high fiving!</h2>
                 <p>
-                  And your eyes are made…for eye contact!
-                  Real humans are so much cooler than their 
-                  tweets or Instagram pictures. 
-                  Tea time has real humans! #nofilter!
-                            
+                  And your eyes are made…for eye contact! Real humans are so
+                  much cooler than their tweets or Instagram pictures. Tea
+                  time has real humans! #nofilter!
                 </p>
               </div>
+            </div>
+          </div>
+        </div>
 
+        <div className="greeting-content-banner">
+          <div className="banner-inner-box">
+            <h1>Actually talk to people.</h1>
+            <Link className="YES" to="/events">
+              Let's Get Tea
+            </Link>
+          </div>
+        </div>
+
+        <div className="greeting-wrapper">
+          <div className="greeting-body">
+
+            <div className="greeting-quotes">
+              <div className="blockquotes-wrap">
+                "I met people who I continue to be in touch with almost a year 
+                later, and people who I shared delightful conversations 
+                with that day but no more. Both are their own kind of fun."
+                 <span className="source">
+                  Freia, NYC
+                </span>
+              </div>
+              <div className="blockquotes-wrap">
+                "I’m not the type of person who talks to strangers. This
+                        doesn’t mean I don’t want to. Tea With Strangers just makes
+                        it easier because you know everyone there wants it too."  
+                <span className="source">  Molly, SF</span>
+              </div>
             </div>
 
+            <div className="greeting-row">
+              <div className="greeting-img fourth" />
+            </div>
+
+            <h3 className="greeting-bottom-header">
+              So why are thousands of people doing it?
+            </h3>
+
+            <div className="greeting-row">
+              <div className="greeting-img fifth" />
+
+              <div className="greeting-writing last">
+                <h2>We won't meet otherwise.</h2>
+                <p>
+                  In all likelihood, our paths won’t cross for any reason.
+                  So often, we only meet people through work, school, or
+                  friends of friends. And even then, it’s questionable. So
+                  basically, we manufacture serendipity
+                </p>
+                <p>
+                  In all likelihood, our paths won’t cross for any reason.
+                  So often, we only meet people through work, school, or
+                  friends of friends. And even then, it’s questionable. So
+                  basically, we manufacture serendipity
+                </p>
+                <p>
+                  In all likelihood, our paths won’t cross for any reason.
+                  So often, we only meet people through work, school, or
+                  friends of friends. And even then, it’s questionable. So
+                  basically, we manufacture serendipity
+                </p>
+              </div>
+            </div>
+
+            
+           
+            
 
           </div>
         </div>
