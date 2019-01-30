f11cf06c06849382b937b431f240976881d013a8
Savio Rodrigues <saviofrod@gmail.com>
2019-01-30 14:05:09 -0800

    Update display content

--- frontend/components/events/event_index_item.jsx
+++ frontend/components/events/event_index_item.jsx
@@ -4,22 +4,31 @@ import {withRouter, Link} from 'react-router-dom';
 
 const eventIndexItem = ({event}) => {
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let utcDate = new Date(event.date);
-    let utcDay = utcDate.getUTCDay();
+    const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL','AUG','SEPT','OCT','NOV','DEC'];
+    let utcFull = new Date(event.date);
+    
+    let utcMonth = utcFull.getUTCMonth(); // 0-12 month of year
+    let utcDate = utcFull.getUTCDate(); // day of month
+    let utcDay = utcFull.getUTCDay(); // 0-7 day of week
+    let utcHour = utcFull.getHours() % 12;
 
-    let displayDay = days[utcDay];
+    let displayMonth = months[utcMonth]; // feb
+    let displayDay = days[utcDay]; // sunday
+
+    let displayDate = displayMonth + ' ' + utcDate; // feb 4
+    let displayAddress = `${event.location}, ${event.city}, CA`;
+
     
     return (
         <div className="event-wrap">
-            <h3>{displayDay}</h3>
-            <h1 className="event-header">Join us for: {event.name}</h1>
+            <h5>{displayDay}</h5>
+            <h3>{displayDate}</h3>
+            <h3> Join us for: {event.name}</h3>
             <ul>
-                <div className="event-date">
-                    <h3>{event.date}</h3>
-                </div>
-                <li>{event.description}</li>
-                <li>{event.location}</li>
-                <li>{event.city}</li>
+                
+                {/* <h5>{event.description}</h5> */}
+                <h4>{displayAddress}</h4>
+                <h4>{utcHour}</h4>
                 <button onClick={() => deleteEvent(event.id)}>Delete</button>
                 <button onClick={() => history.push(`/events/${event.id}/edit`)}>Edit</button>
                 <Link to={`/events/${event.id}`}>show</Link>
