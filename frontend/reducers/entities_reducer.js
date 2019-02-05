6f89b3b10f24ece60fdadbca6c7a045b345e3327
Savio Rodrigues <saviofrod@gmail.com>
2019-02-05 13:20:12 -0800

    Add joins

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import UsersReducer from './users_reducer';
 import EventsReducer from './event_reducer';
+import JoinsReducer from './join_reducer';
 
 
 export default combineReducers({
     users: UsersReducer,
-    events: EventsReducer
+    events: EventsReducer,
+    joins: JoinsReducer
 })
