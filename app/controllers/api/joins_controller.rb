a5c4d99bb0908dd5bb4741256d9bf6539dc5a6c7
Savio Rodrigues <saviofrod@gmail.com>
2019-02-05 13:17:33 -0800

    Initialize joins controller

---
+++ app/controllers/api/joins_controller.rb
@@ -0,0 +1,26 @@
+class Api::JoinsController < ApplicationController
+    before_action :require_login
+
+    def create
+       
+        @join = Join.new(join_params)
+
+        if @join.save
+            render :show
+        else
+            render @join.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @join = Join.find_by(event_id: params[:event_id], user_id: current_user.id)
+
+        @join.destroy!
+        render :show
+    end
+
+    private
+    def join_params
+        params.require(:join).permit(:event_id, :user_id)
+    end
+end
