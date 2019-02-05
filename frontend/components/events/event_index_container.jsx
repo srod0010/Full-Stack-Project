54d4f7f370a6796cd6a986523ea983c4b9ae45ab
Savio Rodrigues <saviofrod@gmail.com>
2019-02-05 13:19:34 -0800

    Update props

--- frontend/components/events/event_index_container.jsx
+++ frontend/components/events/event_index_container.jsx
@@ -5,9 +5,13 @@ import EventIndex from './event_index';
 import {fetchEvents, deleteEvent} from '../../actions/event_actions';
 
 
-const mapStateToProps = state => ({
-    events: Object.values(state.entities.events)
-})
+const mapStateToProps = state => {
+    // debugger
+    return ({
+
+        events: Object.values(state.entities.events)
+    })
+}
 
 const mapDispatchToProps = dispatch => ({
     fetchEvents: () => dispatch(fetchEvents()),
