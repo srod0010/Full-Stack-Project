7a6dfcad63f83d820622a54af27e1b8588bdd084
Savio Rodrigues <saviofrod@gmail.com>
2019-02-05 13:18:28 -0800

    Add create delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
     resources :events, only: [:index, :create, :show, :update, :destroy]
+    resources :joins, only: [:create, :destroy]
   end
 
   root 'static_pages#root'
