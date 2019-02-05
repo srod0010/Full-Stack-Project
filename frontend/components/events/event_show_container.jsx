d88133ae7f8d5a721b91466050b35cf92c5ae6e7
Savio Rodrigues <saviofrod@gmail.com>
2019-02-05 13:19:28 -0800

    Update props

--- frontend/components/events/event_show_container.jsx
+++ frontend/components/events/event_show_container.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import {connect} from 'react-redux';
 import { Link } from 'react-router-dom';
 import EventShow from './event_show';
-import {fetchEvent} from '../../actions/event_actions';
+import {fetchEvent, updateEvent} from '../../actions/event_actions';
+import {createJoin, removeJoin} from '../../actions/join_actions';
 
-const mapStateToProps = (state, ownProps) => ({
-    event: state.entities.events[ownProps.match.params.eventId]
-})
+const mapStateToProps = (state, ownProps) => {
+    let eventId = ownProps.match.params.eventId;
+    let event = state.entities.events[eventId];
+    // let hostId = state.entities.events[eventId].host_id;
+    
+    // debugger;
+    return ({
+        // host: hostId,
+        event: event,
+        currentUserId: state.session.id,
+        users: state.entities.users,
+        join: state.entities.joins,
+        
+        currentUserJoin: Object.values(state.entities.joins).filter(join => join.user_id == state.session.id && join.event_id == eventId)[0]
+    })
+}
 
 const mapDispatchToProps = dispatch => ({
-    fetchEvent: id => dispatch(fetchEvent(id))
-})
+  fetchEvent: id => dispatch(fetchEvent(id)),
+  createJoin: join => dispatch(createJoin(join)),
+  removeJoin: id => dispatch(removeJoin(id)),
+  updateEvent: event => dispatch(updateEvent(event))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventShow);
