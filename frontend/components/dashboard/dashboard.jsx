bc0f1945355318aa3b1862b0c687c2fb587780ec
Savio Rodrigues <saviofrod@gmail.com>
2019-02-12 18:44:50 -0800

    Render joined and hosted events

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import EventIndexItem from '../events/event_index_item';
 
 
 class Dashboard extends React.Component {
@@ -11,12 +12,32 @@ class Dashboard extends React.Component {
         }
     }
 
+    componentDidMount() {
+        this.props.fetchEvents()
+    }
+
     render() {
         if(!this.props.joinedEvents && !this.props.hostedEvents) {
             return <div>NO EVENTS</div>
         }
+        let hosted = this.props.hostedEvents.map(event => {
+            return <EventIndexItem event={event} key={event.id} deleteEvent={this.props.deleteEvent} />
+        })
+
+        let joined = this.props.joinedEvents.map(event => {
+            return <EventIndexItem event={event} key={event.id} deleteEvent={this.props.deleteEvent} />
+        })
+
+        // debugger
         return(
-            <h1>hey</h1>
+            
+            <div>
+                <div>
+                    {hosted}
+                    {joined}
+                </div>
+                <h1>hello</h1>
+            </div>
         )
     }
 
