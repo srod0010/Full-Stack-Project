237c82453853b67ab14f04fdfb4927c89bd74ac1
Savio Rodrigues <saviofrod@gmail.com>
2019-02-18 23:05:41 -0800

    Styling updates

--- frontend/components/progress/progress_bar.jsx
+++ frontend/components/progress/progress_bar.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-import PropTypes from 'prop-types';
-import { isAbsolute } from 'path';
+
 
 export default class ProgressBar extends React.Component {
     
     render() {
-        let sliderColor;
         let newWidth = (100 - (this.props.spots * 20)) + '%';
-        (newWidth === "100%") ? sliderColor = "#8e0000" : sliderColor = "#eda20b";
+        let sliderColor = (newWidth === "100%") ? "#8e0000" : "#eda20b";
 
         const styles = {
             width: `${newWidth}`,
