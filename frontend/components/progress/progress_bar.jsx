680c98133f0728f045a4902e58e1836a847af7f3
Savio Rodrigues <saviofrod@gmail.com>
2019-02-18 22:54:55 -0800

    Change slider color based on seats

--- frontend/components/progress/progress_bar.jsx
+++ frontend/components/progress/progress_bar.jsx
@@ -6,15 +6,18 @@ import { isAbsolute } from 'path';
 export default class ProgressBar extends React.Component {
     
     render() {
+        let sliderColor;
         let newWidth = (100 - (this.props.spots * 20)) + '%';
-    let styles = {
-        width: `${newWidth}`,
-        height: '100%',
-        backgroundColor: "#eda20b",
-        borderRadius: "5px",
-        textAlign: 'center'
-        
-    };
+        (newWidth === "100%") ? sliderColor = "#8e0000" : sliderColor = "#eda20b";
+
+        const styles = {
+            width: `${newWidth}`,
+            height: '100%',
+            backgroundColor: `${sliderColor}`,
+            borderRadius: "5px",
+            textAlign: 'center'
+            
+        };
         
         return (
             <div className="progress" style={styles}></div>
