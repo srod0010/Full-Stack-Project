796494b40c72760410d1a020f94cc311b6272eeb
Savio Rodrigues <saviofrod@gmail.com>
2019-02-28 20:28:54 -0800

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Event.destroy_all
+Join.destroy_all
+
+###images
+img1 = "http://isd-soft.com/wp-content/uploads/2017/02/DCeCHa_logo.png"
+img2 = "https://cdn1.iconfinder.com/data/icons/avatar-2-2/512/Chef_2-512.png"
+img3 = "https://cdn4.iconfinder.com/data/icons/professions-2-2/151/61-512.png"
+img4 = "https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Chef-2-512.png"
+
+
+##host users
+guest = User.create(username: 'Guest', password: 'Guest123', email: 'Guest@guest.com', city: 'San Francisco', image: img4)
+user1 = User.create(username: 'Pablo', password: "password", email:'pablo@email.com', city: 'San Francisco', image: img1)
+user2 = User.create(username: 'Mike', password: "password", email:'mike@email.com', city: 'New York City', image: img2)
+user3 = User.create(username: 'Rick', password: "password", email:'rickandmorty@email.com', city: 'Miami', image: img3)
+###users join
+user4 = User.create(username: 'Savio', password: "password", email:'savio@email.com', city: 'San Francisco', image: img2)
+user5 = User.create(username: 'Jon', password: "password", email:'jon@email.com', city: 'New York City', image: img1)
+user6 = User.create(username: 'Safuh', password: "password", email:'safuh@email.com', city: 'Miami', image: img1)
+user7 = User.create(username: 'Ali', password: "password", email:'ali@email.com', city: 'New York City', image: img3)
+
+###events created
+event1 = Event.create(name: 'Boulevard', date: '2019-03-20',  city: 'San Francisco', location: "401 Berry St", description: "Buzzy, casual eatery for Hawaiian, Indian & Chinese dishes in a brick-walled space with booths.", host_id: guest.id, spots: 0)
+
+event2 = Event.create(name: 'Gary Danko', date: '2019-03-15', city: 'San Francisco', location: "749 9th St", description: "Renowned chef Gary Danko's fixed-price menus of American cuisine in an elegant but unstuffy setting.", host_id: user4.id)
+
+event3 = Event.create(name: 'Tadich Grill', date: '2019-03-24',  city: 'San Francisco', location: "299 South St Pier 36", description: "Californian fare from local ingredients, served in a salvaged-barn-wood space with an open kitchen.", host_id: user5.id)
+
+event4 = Event.create(name: 'Rich Table', date: '2019-03-21',  city: 'New York', location: "Kokkari Estiatorio", description: "Taverna-inspired cuisine served in a candlelit, wood-beamed dining room that lures a chic crowd.", host_id: user2.id)
+
+event5 = Event.create(name: 'Zuni Cafe', date: '2019-03-05', city: 'New York', location: "799 NE 54th St", description: "Crowds eat Californian-Mediterranean fare (& a popular brunch) in an outdoor space screening films.", host_id: user3.id)
+
+###events join
+
+join1 = Join.create(user_id: user4.id, event_id: event1.id)
+join2 = Join.create(user_id: guest.id, event_id: event2.id)
+join3 = Join.create(user_id: user7.id, event_id: event4.id)
+join4 = Join.create(user_id: user5.id, event_id: event4.id)
+join5 = Join.create(user_id: user7.id, event_id: event4.id)
+join6 = Join.create(user_id: user6.id, event_id: event5.id)
+join7 = Join.create(user_id: user2.id, event_id: event3.id)
